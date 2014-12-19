b7d59816f9ca8e74c449447c3e4d506321fa8579
Galen Scovell <galen.scovell@gmail.com>
2014-12-19 15:21:38 -0800

    Begin adding views with dynamic templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+# import Post model from current dir
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    # create variable for QuerySet
+    posts = Post.objects.filter(published_date__isnull=False).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
