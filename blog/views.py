2548b7ebfee7f2a5f240e18886d0c96e66621832
Galen Scovell <galen.scovell@gmail.com>
2014-12-19 21:37:01 -0800

    URLs, Base Templates, Static Files
    This is beginning to rival Sinatra in loveliness.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # import Post model from current dir
 from .models import Post
 
 def post_list(request):
     # create variable for QuerySet
     posts = Post.objects.filter(published_date__isnull=False).order_by('published_date')
+    # render template with 'posts' object as dynamic data
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
