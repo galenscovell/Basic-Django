50661cfcad6d84a3eae579deff1cfe69b70c4012
Galen Scovell <galen.scovell@gmail.com>
2014-12-23 12:21:31 -0800

    Setup PostgreSQL DB
    Well, that was easy. Basically the same as MySQL setup with some changes
    in local_settings.py, should run without a hitch since Heroku is already
    postgres.

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,27 +81,26 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 
+# Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 ALLOWED_HOSTS = ['*']
-
-STATIC_ROOT = 'staticfiles'
-
 DEBUG = False
+LOGIN_REDIRECT_URL = '/'
 
 try:
      from .local_settings import *
 except ImportError:
     pass
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
-LOGIN_REDIRECT_URL = '/'
 
